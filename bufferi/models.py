9c00d28447ea85d5d977c7cfbcc6d9ebf9eefcc8
Sampo Rapeli <35733458+samporapeli@users.noreply.github.com>
2021-05-13 13:50:52 +0300

    Add Customer model

--- bufferi/models.py
+++ bufferi/models.py
@@ -5,10 +5,17 @@ class Product(models.Model):
     name = models.CharField(max_length=140)
     price = models.PositiveIntegerField()
     quantity = models.PositiveIntegerField()
+ 
+
+class Customer(models.Model):
+    name = models.CharField(max_length=140)
+    balance = models.IntegerField()
     deleted = models.BooleanField(default=False)
+    deleted = models.BooleanField(default=False)
+    mobilepay_id = models.CharField(max_length=140, blank=True, null=True)
 
 
 class Transaction(models.Model):
     timestamp = models.DateTimeField()
     product = models.ForeignKey(Product, null=True, on_delete=models.PROTECT)
-    # TODO: ForeignKey to Customer and possibly MP_ID
+    customer = models.ForeignKey(Customer, null=True, on_delete=models.PROTECT)
