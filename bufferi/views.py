c3b31f1b15fd95a7f5b52a1dfba6b139d15771b5
Joel Juhela <joel.juhela@gmail.com>
2021-05-13 14:00:08 +0300

    Add product API

--- bufferi/views.py
+++ bufferi/views.py
@@ -1,7 +1,14 @@
 from django.shortcuts import render
+from rest_framework import viewsets
+from .models import Product
+from .serializers import ProductSerializer
 
 
 def index(request):
     """Serves Vue frontend"""
     return render(request, template_name="index.html")
 
+
+class ProductViewSet(viewsets.ModelViewSet):
+    queryset = Product.objects.filter(deleted=False)
+    serializer_class = ProductSerializer
