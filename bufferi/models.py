093499edef0db25f39f03a499fec64cefd8d024d
Joel Juhela <joel.juhela@gmail.com>
2021-05-13 15:57:27 +0300

    Add better str-functions to models

--- bufferi/models.py
+++ bufferi/models.py
@@ -6,6 +6,9 @@ class Product(models.Model):
     price = models.PositiveIntegerField()
     quantity = models.PositiveIntegerField()
     deleted = models.BooleanField(default=False)
+
+    def __str__(self):
+        return f"{self.name} {(self.price/100):.2f}€ {self.quantity} kpl"
  
 
 class Customer(models.Model):
@@ -13,6 +16,9 @@ class Customer(models.Model):
     balance = models.IntegerField()
     deleted = models.BooleanField(default=False)
     mobilepay_id = models.CharField(max_length=140, blank=True, null=True)
+
+    def __str__(self):
+        return self.name
 
 
 class Transaction(models.Model):
