f5cd7aacd585d492d519aa0ab772e926f0850288
Joel Juhela <joel.juhela@gmail.com>
2021-05-13 11:10:59 +0300

    Add Product and Transaction models to bufferi

--- bufferi/models.py
+++ bufferi/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
-# Create your models here.
+
+class Product(models.Model):
+    name = models.CharField(max_length=140)
+    price = models.PositiveIntegerField()
+    quantity = models.PositiveIntegerField()
+    deleted = models.BooleanField(default=False)
+
+
+class Transaction(models.Model):
+    timestamp = models.DateTimeField()
+    product = models.ForeignKey(Product, null=True, on_delete=models.PROTECT)
+    # TODO: ForeignKey to Customer and possibly MP_ID
